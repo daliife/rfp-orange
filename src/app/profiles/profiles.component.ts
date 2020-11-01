3e3b968dd45244ebb8234921069f503f1781215c
daliife <compresdavid@gmail.com>
2020-11-01 23:27:48 +0100

    fix: profile logo tweak

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -12,18 +12,36 @@ export class ProfilesComponent implements OnInit {
       description: 'blablabla',
       imgPath: 'assets/img/profiles/img-user-research@2x.png',
       color: '#ff8ad4',
+      stats:{
+        experience: 4,
+        empathy: 3,
+        analitical: 2,
+        objectivity: 1,
+      }
     },
     {
       title: 'User Research',
       description: 'blablabla',
       imgPath: 'assets/img/profiles/img-ux-interaction@2x.png',
       color: '#50be87',
+      stats:{
+        experience: 4,
+        empathy: 3,
+        analitical: 2,
+        objectivity: 1,
+      }
     },
     {
       title: 'User Research',
       description: 'blablabla',
       imgPath: 'assets/img/profiles/img-experience-design@2x.png',
       color: '#4bb4e6',
+      stats:{
+        experience: 4,
+        empathy: 3,
+        analitical: 2,
+        objectivity: 1,
+      }
     },
   ];
 
