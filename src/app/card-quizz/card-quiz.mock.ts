42123b2d9f128330ad7ac59681ea6fbf836e4449
David G <compresdavid@gmail.com>
2020-11-02 18:03:42 +0100

    feat: initial mini-quiz structure

---
+++ src/app/card-quizz/card-quiz.mock.ts
@@ -0,0 +1,59 @@
+export const questions = [
+  {
+    type: 0,
+    question:
+      '111111¿Qué técnica utilizamos para conceptualizar la experiencia del cliente en las fases más tempranas?',
+    answers: [
+      'a) Árbol de contenido',
+      'b) Customer journey map',
+      'c) Prototipo de bajo nivel',
+      'd) Mapas de empatía',
+    ],
+    solution: 'd) Mapas de empatía',
+    reason:
+      'Esta actividad nos permite ponernos en los zapatos de los usuarios y entender el contexto de sus necesidades/problemas.​',
+    advice:
+      'Colocar una imagen de un usuario en el centro de la herramienta ayuda a crear cercanía y empatizar.',
+    adviceLabel: 'advice',
+    frontImagePath: 'test.png',
+    backImagePath: 'test.png',
+  },
+  {
+    type: 0,
+    question:
+      '22222¿Qué técnica utilizamos para conceptualizar la experiencia del cliente en las fases más tempranas?',
+    answers: [
+      'a) Árbol de contenido',
+      'b) Customer journey map',
+      'c) Prototipo de bajo nivel',
+      'd) Mapas de empatía',
+    ],
+    solution: 'd) Mapas de empatía',
+    reason:
+      'Esta actividad nos permite ponernos en los zapatos de los usuarios y entender el contexto de sus necesidades/problemas.​',
+    advice:
+      'Colocar una imagen de un usuario en el centro de la herramienta ayuda a crear cercanía y empatizar.',
+    adviceLabel: 'advice',
+    frontImagePath: 'test.png',
+    backImagePath: 'test.png',
+  },
+  {
+    type: 0,
+    question:
+      '33333¿Qué técnica utilizamos para conceptualizar la experiencia del cliente en las fases más tempranas?',
+    answers: [
+      'a) Árbol de contenido',
+      'b) Customer journey map',
+      'c) Prototipo de bajo nivel',
+      'd) Mapas de empatía',
+    ],
+    solution: 'd) Mapas de empatía',
+    reason:
+      'Esta actividad nos permite ponernos en los zapatos de los usuarios y entender el contexto de sus necesidades/problemas.​',
+    advice:
+      'Colocar una imagen de un usuario en el centro de la herramienta ayuda a crear cercanía y empatizar.',
+    adviceLabel: 'advice',
+    frontImagePath: 'test.png',
+    backImagePath: 'test.png',
+  },
+];
--- src/app/card-quizz/card-quizz.component.ts
+++ src/app/card-quizz/card-quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { questions } from './card-quiz.mock';
 
 @Component({
   selector: 'app-card-quizz',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardQuizzComponent implements OnInit {
 
+  isFront = true;
+  cardId = 0;
+  questionsList = questions;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  toggleCard(): void{
+    this.isFront = !this.isFront;
   }
+
+  nextCard(): void{
+    this.isFront = true;
+    if (this.cardId < this.questionsList.length - 1) {
+      this.cardId++;
+    }else{
+      this.cardId = 0;
+    }
+  }
+
+  previousCard(): void{
+    this.isFront = true;
+    if (this.cardId >= 1) {
+      this.cardId--;
+    }else{
+      this.cardId = this.questionsList.length - 1;
+    }
+  }
+
 
 }
