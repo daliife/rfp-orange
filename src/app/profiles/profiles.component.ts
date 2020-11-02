1159ee11c1b6fc12420dc30f5c3c1e9d1fe49fd3
David G <compresdavid@gmail.com>
2020-11-02 17:11:23 +0100

    feat: modular profiles stat + fix styles and anims

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -8,40 +8,91 @@ import { Component, OnInit } from '@angular/core';
 export class ProfilesComponent implements OnInit {
   profiles: any[] = [
     {
-      title: 'User Research',
+      title: 'User Research Expert',
       description: 'blablabla',
       imgPath: 'assets/img/profiles/img-user-research@2x.png',
       color: '#ff8ad4',
-      stats:{
-        experience: 4,
-        empathy: 3,
-        analitical: 2,
-        objectivity: 1,
-      }
+      downloadLink: 'https://google.com',
+      stats: [
+        {
+          name: 'Experiencia',
+          imgPath: 'assets/img/profiles/img-user-research@2x.png',
+          value: 1,
+        },
+        {
+          name: 'Empatía',
+          imgPath: 'assets/img/profiles/img-user-research@2x.png',
+          value: 2,
+        },
+        {
+          name: 'Analítico',
+          imgPath: 'assets/img/profiles/img-user-research@2x.png',
+          value: 3,
+        },
+        {
+          name: 'Objetividad',
+          imgPath: 'assets/img/profiles/img-user-research@2x.png',
+          value: 4,
+        },
+      ],
     },
     {
-      title: 'User Research',
+      title: 'UX Interaction Expert',
       description: 'blablabla',
       imgPath: 'assets/img/profiles/img-ux-interaction@2x.png',
       color: '#50be87',
-      stats:{
-        experience: 4,
-        empathy: 3,
-        analitical: 2,
-        objectivity: 1,
-      }
+      downloadLink: 'https://google.com',
+      stats: [
+        {
+          name: 'Experiencia',
+          imgPath: 'assets/img/profiles/img-ux-interaction@2x.png',
+          value: 4,
+        },
+        {
+          name: 'Empatía',
+          imgPath: 'assets/img/profiles/img-ux-interaction@2x.png',
+          value: 3,
+        },
+        {
+          name: 'Creatividad',
+          imgPath: 'assets/img/profiles/img-ux-interaction@2x.png',
+          value: 2,
+        },
+        {
+          name: 'Comunicación',
+          imgPath: 'assets/img/profiles/img-ux-interaction@2x.png',
+          value: 2,
+        },
+      ],
     },
     {
-      title: 'User Research',
+      title: 'Experience Design Expert',
       description: 'blablabla',
       imgPath: 'assets/img/profiles/img-experience-design@2x.png',
       color: '#4bb4e6',
-      stats:{
-        experience: 4,
-        empathy: 3,
-        analitical: 2,
-        objectivity: 1,
-      }
+      downloadLink: 'https://google.com',
+      stats: [
+        {
+          name: 'Experiencia',
+          imgPath: 'assets/img/profiles/img-experience-design@2x.png',
+          value: 4,
+        },
+        {
+          name: 'Analítico',
+          imgPath: 'assets/img/profiles/img-experience-design@2x.png',
+          value: 3,
+        },
+        {
+          name: 'Creatividad',
+          imgPath: 'assets/img/profiles/img-experience-design@2x.png',
+          value: 2,
+        },
+        {
+          name: 'Innovación',
+          imgPath: 'assets/img/profiles/img-experience-design@2x.png',
+          value: 2,
+        },
+      ],
     },
   ];
 
